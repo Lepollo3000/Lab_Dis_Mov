c37770921b28b9e562ad21fc25855f269af1bd2b
LAP_LEPECIANA\josem <Jos Lepe>
2024-10-06 21:36:40 -0600

    Implementada localización para traducciones en la aplicación.

--- PRUEBA/Client/Program.cs
+++ PRUEBA/Client/Program.cs
@@ -1,7 +1,12 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 using PRUEBA.Client;
+using PRUEBA.Client.Shared;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,6 +18,22 @@ builder.Services.AddHttpClient("PRUEBA.ServerAPI", client => client.BaseAddress 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("PRUEBA.ServerAPI"));
 
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
+builder.Configuration.AddJsonFile("appsettings.json", optional: true,
+  reloadOnChange: true);
+
+//builder.Services.AddLocalization(opts => { opts.ResourcesPath = "Resources"; });
+
 builder.Services.AddApiAuthorization();
+
+// 1. Add the LocalStorage Services
+builder.Services.AddBlazoredLocalStorage();
+// 2. Add the Localization Service
+builder.Services.AddLocalization();
+
+var host = builder.Build();
+
+// 3. Set the Default UI Culture
+await host.SetDefaultUICulture();
 
 await builder.Build().RunAsync();
